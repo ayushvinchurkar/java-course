7242c60d04b8d62960380fb6237a2ff44c478eda
ayushvinchurkar <ayushvinchurkar10@gmail.com>
2024-12-04 21:01:40 +0530

    hey its my first repo

---
+++ cwh_17_logical.java
@@ -0,0 +1,35 @@
+
+public class cwh_17_logical {
+    public static void main(String[] args) {
+        //System.out.println("For Logical AND...");
+        boolean a = false;
+        boolean b = true;
+
+//        if (a && b){
+//            System.out.println("Y");
+//
+//        }
+//        else {
+//            System.out.println("N");
+//        }
+
+//        System.out.println("For Logical OR....");
+//
+//        if (a || b){
+//            System.out.println("Y");
+//        }
+//        else{
+//            System.out.println("N");
+//        }
+
+        System.out.println("For Logical NOT.....");
+        System.out.println("Not(a) is : ");
+        System.out.println(!a);
+        System.out.println("Not(b) is : ");
+        System.out.println(!b);
+
+
+
+
+    }
+}
